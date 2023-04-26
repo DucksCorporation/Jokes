1cfc18f0623722d996368e1037680b64ec5b5e80
Picsou <111159871+Piccsou@users.noreply.github.com>
2023-04-26 22:06:09 +0200

    Update index.js

--- all_jokes/index.js
+++ all_jokes/index.js
@@ -1,32 +1,50 @@
 //@ts-check
-const jokesType = ["dark", "limit", "dev", "normal", "beauf"];
-const file = require("./Bdd/jokes.json");
+const file = require("./jokes.json");
+const JokeType = ["dark", "limit", "dev", "normal", "beauf", "blonde"];
 
 module.exports.Jokes = class Jokes {
-    getRandomJoke() {    
+    getRandomJoke() {
         return file[Math.floor(Math.random() * file.length)];
     };
 
     getJokeByType(type) {
-        if(!jokesType.includes(type)) throw new TypeError("Argument is not valid.");
+        if(!JokeType.includes(type)) throw new TypeError("Argument is not valid.");
 
-        const f = file.filter(e => e.type === type);
+        const f = this.filterJoke(type.toString());
         return f[Math.floor(Math.random() * f.length)];
     };
 
     getJokeById(id) {
-        if(isNaN(id)) throw new TypeError("Argument is not valid.");
+        const maxId = file[file.length - 1].id;
+
+        if(isNaN(id)) throw new TypeError("Argument must be a number.");
         if(!Number.isInteger(id)) throw new TypeError("Argument must be an integer.");
-        if(id <= 0 ) throw new RangeError("Argument must be upper than 0.");
-        if(id === Infinity) throw new RangeError("Argument must be lower than Infinity.");
+        if(id < 0 || id > maxId || id === 0) throw new RangeError(`Argument must be upper than 0 and lower than ${maxId}`); //I know, this is weird but I have ID problem in jokes.json
         
         return file.find(e => e.id === id);
     };
 
-    help() {
+    getStats() {
+        const data = {
+            numberOfJokes: file.length,
+            DarkJokes: this.filterJoke("dark").length,
+            LimitJokes: this.filterJoke("limit").length,
+            DevJokes: this.filterJoke("dev").length,
+            NormalJokes: this.filterJoke("normal").length,
+            BeaufJokes: this.filterJoke("beauf").length,
+            BlondesJokes: this.filterJoke("blonde").length
+        };
+
+        return data;
+    };
+
+    getHelp() {
         return "https://discord.gg/E5hPRnMUgq";
     };
-    getJokeCount() {
-        return file.length;
+
+    filterJoke(type) {
+        if(!JokeType.includes(type)) throw new TypeError("[Internal error] type error in filterJoke().");
+
+        return file.filter(e => e.type === type);
     };
 };
